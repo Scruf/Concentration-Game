5632b2232b9b39a4063d2b90c2ad105aa4941020
scruf <ek5442@cs.rit.edu>
2015-11-22 17:19:18 -0500

    Partial implemetation of Grid with cards completed Queue need further revision

--- GViewControl.java
+++ GViewControl.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
+import java.util.LinkedList;
 import java.util.Observable;
 import java.util.ArrayList;
-
+import java.util.Queue;
 /**
  * Created by scruf on 11/20/15.
  */
@@ -13,19 +13,22 @@ public class GViewControl extends javax.swing.JFrame implements  java.util.Obser
     public static Observable observer;
     public boolean firstCard = false;
     public static JLabel label;
+    public static GViewControl gViewControl;
+    public static ArrayList<JButton> buttons;
+    public static Queue<JButton>  selected = new LinkedList<JButton>();
     public GViewControl(ConcentrationModel  model){
         this.concentration = model;
     }
 
     public static void main(String []args){
         observer = new Observable();
-        GViewControl gViewControl = new GViewControl(new ConcentrationModel());
+        gViewControl = new GViewControl(new ConcentrationModel());
         JButton reset,cheat,undo;
         label = new JLabel("Moves: 0 Select the first card.");
         reset = new JButton("Reset");
         cheat = new JButton("Cheat");
         undo = new JButton("Undo");
-        ArrayList<JButton> buttons = new ArrayList<JButton>();
+        buttons = new ArrayList<JButton>();
         //create empty 16 buttons
         //change their size to 100x100
         //set each button name to a corresponding index
@@ -65,7 +68,7 @@ public class GViewControl extends javax.swing.JFrame implements  java.util.Obser
 
             btn.addActionListener(e->{
               gViewControl.concentration.selectCard(Integer.parseInt(btn.getName()));
-                System.out.println(gViewControl.concentration.getCards());
+                System.out.println(gViewControl.concentration.getCards()+"THere are "+gViewControl.concentration.howManyCardsUp());
                 CardButton button = new CardButton(Integer.parseInt(btn.getName()));
                 gViewControl.update(null,new ActionPerformed(btn,Integer.parseInt(btn.getName())));
                /* btn.setText(String.valueOf(gViewControl.concentration.getCards().get(Integer.parseInt(btn.getName()))));*/
@@ -74,12 +77,48 @@ public class GViewControl extends javax.swing.JFrame implements  java.util.Obser
         }
 
     }
-
+    public String parseButton(String temp){
+        return "-"+temp+"-";
+    }
 
     @Override
     public void update(Observable observable, Object o) {
+        ActionPerformed action = (ActionPerformed)o;
+        JButton btn = action.getButton();
+        if(selected.size()<2)
+            selected.add(btn);
+        if(selected.size()==2){
+            JButton first;
+            JButton second;
 
+                first=selected.poll();
+                second=selected.poll();
+            if(first.getText().equals(second.getText())){
+                first.setBackground(Color.GREEN);
+                second.setBackground(Color.GREEN);
+            }
+            else{
+                first.setBackground(Color.WHITE);
+                second.setBackground(Color.WHITE);
+                first.setText(" ");
+                second.setText(" ");
+            }
+            selected.clear();
+
+        }
+        String text = String.valueOf(gViewControl.concentration.getCards().get(action.getPos()));
+        btn.setText(text);
+        firstCard=!firstCard;
+        if(firstCard){
+            label.setText("Moves: "+gViewControl.concentration.getMoveCount()+" Select the first card. ");
+
+        }
+        else{
+            label.setText("Moves: "+gViewControl.concentration.getMoveCount()+" "+"Select the second card.");
+        }
+
+        }
 
     }
-}
+
 
