bee4d162b1400dfb1769b417e3718a58f1dff774
scruf <ek5442@cs.rit.edu>
2015-11-22 12:08:20 -0500

    Buttons modified according to a standart

--- GViewControl.java
+++ GViewControl.java
@@ -6,31 +6,35 @@ import java.util.ArrayList;
 /**
  * Created by scruf on 11/20/15.
  */
+
 public class GViewControl extends javax.swing.JFrame implements  java.util.Observer{
+    public ConcentrationModel concentration;
     public GViewControl(ConcentrationModel  model){
-        model = new ConcentrationModel();
+        this.concentration = model;
     }
 
     public static void main(String []args){
 
+        GViewControl gViewControl = new GViewControl(new ConcentrationModel());
         JButton reset,cheat,undo;
         reset = new JButton("Reset");
         cheat = new JButton("Cheat");
         undo = new JButton("Undo");
         ArrayList<JButton> buttons = new ArrayList<JButton>();
-        /*Button layout*/
 
-        for(int i=0;i<8;i++)
-            buttons.add(new JButton(String.valueOf(i)));
-        for(int i=0;i<8;i++)
-            buttons.add(new JButton(String.valueOf(i)));
-        for(JButton btn  : buttons){
-            btn.setPreferredSize(new Dimension(100,100));
+        for(int i=0;i<16;i++){
+            buttons.add(new JButton(" "));
         }
 
-        /*
-        * Frame layout
-        * */
+
+
+        for(JButton btn  : buttons){
+            btn.setPreferredSize(new Dimension(100, 100));
+
+        }
+
+
+
         JFrame frame = new JFrame();
         frame.setSize(500, 500);
         frame.setTitle("Egor Kozitski");
@@ -43,6 +47,10 @@ public class GViewControl extends javax.swing.JFrame implements  java.util.Obser
         panel.add(undo);
         panel.add(reset);
         frame.add(panel);
+
+
+
+
 
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
